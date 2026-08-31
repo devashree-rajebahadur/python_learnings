ee71c0ba19698933bf543d86cc0c1b3151097421
devashree-rajebahadur <devashreeraje05@gmail.com>
2026-08-31 22:38:07 +0530

    Create ex1_bodmas.py

---
+++ ex1_bodmas.py
@@ -0,0 +1,32 @@
+x = 277 
+y = 13 
+
+#Division 
+a = x / y 
+print(a)
+
+#Multiplication
+b = x * y
+print(b)
+
+#Addition
+c = x + y
+print(c)
+
+#Subtraction
+d = x - y
+print(d)
+
+#Brackets
+e = (x - y) * (b + c) * (x + y) 
+print(e)
+
+#Exponentiation
+f = x ** y
+print(f)
+
+#Modulus
+g = x % y
+print(g)
+
+
