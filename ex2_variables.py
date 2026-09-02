7af447289971ed11c5ade9d2dc283d333f771063
devashree-rajebahadur <devashreeraje05@gmail.com>
2026-09-02 23:09:35 +0530

    Create ex2_variables.py

---
+++ ex2_variables.py
@@ -0,0 +1,31 @@
+#variables in python are like containers that store data values
+
+#variable names can start with a letter or an underscore but not with a number
+a = 241 
+_b = 124
+
+#variable names are case sensitive
+A = "two hundred and forty one" 
+#A and a are two different variables
+
+#mathematical operations can be performed on variables 
+c = a + _b 
+d = a / _b
+print(c)
+print (d)
+
+#the class/type of a variable can be found out using the type() function 
+print("type:", type(A))
+
+#string variables can be concatanated using + 
+B = " bananas"
+print(A + B)
+
+
+
+
+
+
+
+
+
